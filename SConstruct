be4274d3347aff8076f8e62eff05398c7cb4e5be
i.vetrov <qprostu@gmail.com>
2018-01-27 12:45:02 +0300

    Read ip addresses and ports from configuration

--- SConstruct
+++ SConstruct
@@ -5,7 +5,6 @@ SQLProxy server build configuration
 env = Environment(
     CPPFLAGS='-m64 -pipe -O3 -g -Wall -W -std=c++11',
     LINKFLAGS='-m64 -g -Wl,-O3',
-    CPPDEFINES='-D_DEBUG',
     CPPPATH=['/usr/local/include'],
     LIBPATH=['/usr/local/lib'],
     LIBS=Split('pthread boost_system boost_program_options boost_thread '
--- sqlproxy.cpp
+++ sqlproxy.cpp
@@ -1,12 +1,20 @@
+#include <string>
 #include <iostream>
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
+#include <boost/property_tree/info_parser.hpp>
+#include <boost/lexical_cast.hpp>
 
 
-namespace {
-    namespace fs = boost::filesystem;
-    namespace po = boost::program_options;
+namespace pt = boost::property_tree;
+namespace fs = boost::filesystem;
+namespace po = boost::program_options;
 
+
+namespace sqlproxy {
+    /**
+     * Initialization
+     */
     void init_all(int argc, char **argv) {
         po::options_description desc("SQLProxy command line options");
         std::string config;
@@ -30,13 +38,26 @@ namespace {
         if (!fs::exists(config)) {
             throw std::runtime_error(config + " does not exist...");
         }
+
+        pt::ptree proxy_config;
+        std::ifstream in(config);
+        pt::info_parser::read_info(in, proxy_config);
+        in.close();
+
+        std::string local_ip = proxy_config.get<std::string>("SQLProxy.proxy_ip");
+        uint16_t local_port = proxy_config.get<int>("SQLProxy.proxy_port");
+        std::string server_ip = proxy_config.get<std::string>("SQLProxy.postgresql_ip");
+        uint16_t server_port = proxy_config.get<int>("SQLProxy.postgresql_port");
+
+        std::cout << "local: " << local_ip << ":" << local_port << ", "
+                  << "remote: " <<  server_ip << ":" << server_port << "\n";
     }
-}
+}  // namespace sqlproxy
 
 
 int main(int argc, char **argv) {
     try {
-        init_all(argc, argv);
+        sqlproxy::init_all(argc, argv);
     } catch (po::required_option &err) {
         std::cerr << "  error: " << err.what() << "\n";
     } catch (std::exception &e) {
