155ad66b314892de71142e6a86459f440421e04f
i.vetrov <qprostu@gmail.com>
2018-01-27 19:23:29 +0300

    Proxy class for requests to PostgreSQL

--- sqlproxy.cpp
+++ sqlproxy.cpp
@@ -1,3 +1,8 @@
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread/mutex.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 #include <boost/property_tree/info_parser.hpp>
@@ -13,76 +18,221 @@
 namespace pt = boost::property_tree;
 namespace fs = boost::filesystem;
 namespace po = boost::program_options;
+namespace ip = boost::asio::ip;
 
 using boost::asio::ip::tcp;
+
 
 namespace sqlproxy {
 
     class session
-        : public std::enable_shared_from_this<session>
+        : public boost::enable_shared_from_this<session>
     {
     public:
-        session(tcp::socket socket)
-            : socket_(std::move(socket))
+        typedef boost::shared_ptr<session> ptr_type;
+
+        session(boost::asio::io_service& ios)
+            : client_socket_(ios),
+              server_socket_(ios)
         {}
 
-        void start() {
-            do_read();
+        tcp::socket& client_socket() {
+            return client_socket_;
+        }
+
+        void start(const std::string& server_host,
+                   uint16_t server_port) {
+            // Attempt to connect to PostgreSQL server
+            server_socket_.async_connect(
+                tcp::endpoint(
+                    ip::address::from_string(server_host),
+                    server_port),
+                boost::bind(
+                    &session::on_server_connect,
+                    shared_from_this(),
+                    boost::asio::placeholders::error));
         }
 
     private:
-        void do_read() {
-            auto self(shared_from_this());
-            socket_.async_read_some(
-                boost::asio::buffer(data_, max_length),
-                [this, self](boost::system::error_code ec, std::size_t length) {
-                    if (!ec) {
-                        std::cout << data_ << "\n";
-                        do_write(length);
-                    }
-                });
+        // Server connected
+        void on_server_connect(
+                const boost::system::error_code& ec) {
+            if (!ec) {
+                auto self(shared_from_this());
+                // Async read from PostgreSQL server
+                server_socket_.async_read_some(
+                    boost::asio::buffer(server_data_, max_length),
+                    boost::bind(
+                        &session::on_server_read,
+                        self,
+                        boost::asio::placeholders::error,
+                        boost::asio::placeholders::bytes_transferred));
+
+                // Async read from client
+                client_socket_.async_read_some(
+                    boost::asio::buffer(client_data_, max_length),
+                    boost::bind(
+                        &session::on_client_read,
+                        self,
+                        boost::asio::placeholders::error,
+                        boost::asio::placeholders::bytes_transferred));
+            }
+            else {
+                close();
+            }
         }
 
-        void do_write(std::size_t length) {
-            auto self(shared_from_this());
-            boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
-                [this, self](boost::system::error_code ec, std::size_t /*length*/) {
-                    if (!ec) {
-                        do_read();
-                    }
-                });
+        // Send data from PostgreSQL server to client
+        void on_server_read(const boost::system::error_code& ec,
+                          const size_t& bytes_transferred) {
+            if (!ec) {
+                async_write(
+                    client_socket_,
+                    boost::asio::buffer(server_data_, bytes_transferred),
+                    boost::bind(&session::on_client_write,
+                                shared_from_this(),
+                                boost::asio::placeholders::error));
+            }
+            else {
+                close();
+            }
         }
 
-        tcp::socket socket_;
-        enum { max_length = 1024 };
-        char data_[max_length];
-    };
+        // Async read from PostgreSQL server
+        void on_client_write(const boost::system::error_code& error) {
+            if (!error) {
+                server_socket_.async_read_some(
+                    boost::asio::buffer(server_data_, max_length),
+                    boost::bind(
+                        &session::on_server_read,
+                        shared_from_this(),
+                        boost::asio::placeholders::error,
+                        boost::asio::placeholders::bytes_transferred));
+            }
+            else {
+                close();
+            }
+        }
+
+        // Send data to PostgreSQL server
+        void on_client_read(const boost::system::error_code& ec,
+                            const size_t& bytes_transferred) {
+            if (!ec) {
+                std::cout << static_cast<char>(client_data_[0]) << " ";
+                uint64_t p = (client_data_[1] << 24) +
+                             (client_data_[2] << 16) +
+                             (client_data_[3] << 8) +
+                             client_data_[4];
+                std::cout << p << " ";
+                std::cout << std::string(
+                    reinterpret_cast<const char*>(&client_data_[5]),
+                    bytes_transferred - 5) << "\n";
+                async_write(
+                    server_socket_,
+                    boost::asio::buffer(client_data_, bytes_transferred),
+                    boost::bind(&session::on_server_write,
+                                shared_from_this(),
+                                boost::asio::placeholders::error));
+            }
+            else {
+                close();
+            }
+        }
+
+        // Async read from client
+        void on_server_write(const boost::system::error_code& ec) {
+            if (!ec) {
+                client_socket_.async_read_some(
+                    boost::asio::buffer(client_data_, max_length),
+                    boost::bind(
+                        &session::on_client_read,
+                        shared_from_this(),
+                        boost::asio::placeholders::error,
+                        boost::asio::placeholders::bytes_transferred));
+            }
+            else {
+                close();
+            }
+        }
+
+        // Close client and server sockets
+        void close() {
+            boost::mutex::scoped_lock lock(mutex_);
+
+            if (client_socket_.is_open()) {
+                client_socket_.close();
+            }
+
+            if (server_socket_.is_open()) {
+                server_socket_.close();
+            }
+        }
+
+        tcp::socket client_socket_;
+        tcp::socket server_socket_;
+
+        enum { max_length = 4096 };
+        std::array<uint8_t, max_length> client_data_;
+        std::array<uint8_t, max_length> server_data_;
+
+        boost::mutex mutex_;
+    };  // class session
 
     class server
     {
     public:
-        server(boost::asio::io_service& io_service, short port)
-            : acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
-              socket_(io_service)
-        {
-            do_accept();
+        server(boost::asio::io_service& io_service,
+               const std::string& local_host, uint16_t local_port,
+               const std::string& server_host, uint16_t server_port)
+            : io_service_(io_service),
+              localhost_address_(
+                  boost::asio::ip::address_v4::from_string(local_host)),
+              acceptor_(io_service_,
+                        tcp::endpoint(localhost_address_, local_port)),
+              server_port_(server_port),
+              server_host_(server_host)
+        {}
+
+        bool accept_connections() {
+            try {
+                session_ = boost::make_shared<session>(io_service_);
+
+                acceptor_.async_accept(
+                    session_->client_socket(),
+                    boost::bind(&server::on_accept,
+                                this,
+                                boost::asio::placeholders::error));
+            }
+            catch(std::exception& e) {
+                std::cerr << "Server exception: " << e.what() << "\n";
+                return false;
+            }
+
+            return true;
         }
 
     private:
-        void do_accept() {
-            acceptor_.async_accept(socket_,
-                [this](boost::system::error_code ec) {
-                    if (!ec) {
-                        std::make_shared<session>(std::move(socket_))->start();
-                    }
+        // On accept connection
+        void on_accept(const boost::system::error_code& ec) {
+            if (!ec) {
+                session_->start(server_host_, server_port_);
 
-                    do_accept();
-                });
+                if (!accept_connections()) {
+                    std::cerr << "Failure during call to accept.\n";
+                }
+            }
+            else {
+                std::cerr << "Error: " << ec.message() << "\n";
+            }
         }
 
+        boost::asio::io_service& io_service_;
+        ip::address_v4 localhost_address_;
         tcp::acceptor acceptor_;
-        tcp::socket socket_;
-    };
+        session::ptr_type session_;
+        uint16_t server_port_;
+        std::string server_host_;
+    };  // class server
 
     /**
      * Initialization
@@ -125,8 +275,12 @@ namespace sqlproxy {
                   << "remote: " <<  server_ip << ":" << server_port << "\n";
 
         boost::asio::io_service io_service;
+        sqlproxy::server server(
+            io_service,
+            local_ip, local_port,
+            server_ip, server_port);
 
-        server s(io_service, local_port);
+        server.accept_connections();
         io_service.run();
     }
 }  // namespace sqlproxy
