b81f8602ed20149ae7e9ffe7603643f190545a08
i.vetrov <qprostu@gmail.com>
2018-01-27 16:48:29 +0300

    Simple boost echo server from examples

--- sqlproxy.cpp
+++ sqlproxy.cpp
@@ -1,17 +1,89 @@
-#include <string>
-#include <iostream>
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 #include <boost/property_tree/info_parser.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/asio.hpp>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <memory>
+#include <utility>
 
 
 namespace pt = boost::property_tree;
 namespace fs = boost::filesystem;
 namespace po = boost::program_options;
 
+using boost::asio::ip::tcp;
 
 namespace sqlproxy {
+
+    class session
+        : public std::enable_shared_from_this<session>
+    {
+    public:
+        session(tcp::socket socket)
+            : socket_(std::move(socket))
+        {}
+
+        void start() {
+            do_read();
+        }
+
+    private:
+        void do_read() {
+            auto self(shared_from_this());
+            socket_.async_read_some(
+                boost::asio::buffer(data_, max_length),
+                [this, self](boost::system::error_code ec, std::size_t length) {
+                    if (!ec) {
+                        std::cout << data_ << "\n";
+                        do_write(length);
+                    }
+                });
+        }
+
+        void do_write(std::size_t length) {
+            auto self(shared_from_this());
+            boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+                [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                    if (!ec) {
+                        do_read();
+                    }
+                });
+        }
+
+        tcp::socket socket_;
+        enum { max_length = 1024 };
+        char data_[max_length];
+    };
+
+    class server
+    {
+    public:
+        server(boost::asio::io_service& io_service, short port)
+            : acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
+              socket_(io_service)
+        {
+            do_accept();
+        }
+
+    private:
+        void do_accept() {
+            acceptor_.async_accept(socket_,
+                [this](boost::system::error_code ec) {
+                    if (!ec) {
+                        std::make_shared<session>(std::move(socket_))->start();
+                    }
+
+                    do_accept();
+                });
+        }
+
+        tcp::acceptor acceptor_;
+        tcp::socket socket_;
+    };
+
     /**
      * Initialization
      */
@@ -51,6 +123,11 @@ namespace sqlproxy {
 
         std::cout << "local: " << local_ip << ":" << local_port << ", "
                   << "remote: " <<  server_ip << ":" << server_port << "\n";
+
+        boost::asio::io_service io_service;
+
+        server s(io_service, local_port);
+        io_service.run();
     }
 }  // namespace sqlproxy
 
@@ -59,8 +136,8 @@ int main(int argc, char **argv) {
     try {
         sqlproxy::init_all(argc, argv);
     } catch (po::required_option &err) {
-        std::cerr << "  error: " << err.what() << "\n";
+        std::cerr << "Exception: " << err.what() << "\n";
     } catch (std::exception &e) {
-        std::cerr << "  error:" << e.what() << "\n";
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 }
